a86ec5ef2412a94ea6aa0ccb57b33ad8c89ce90c
Leland Weathers <97176164+lelandweathers@users.noreply.github.com>
2022-01-09 14:21:20 -0600

    explicitly set pipeline account/region

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-from os import getenv
-
 import aws_cdk as cdk
 from dotenv import load_dotenv
 
@@ -12,9 +10,7 @@ app = cdk.App()
 PipelineStack(
     app,
     "VideoManagementPipelineStack",
-    env=cdk.Environment(
-        account=getenv("AWS_PIPELINE_ACCOUNT"), region=getenv("AWS_PIPELINE_REGION")
-    ),
+    env=cdk.Environment(account="837883156427", region="us-east-2"),
 )
 
 app.synth()
