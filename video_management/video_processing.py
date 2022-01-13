feef1196e448205401a66fac7a19d08c5195ae3e
Leland Weathers <97176164+lelandweathers@users.noreply.github.com>
2022-01-13 09:56:33 -0600

    use existing logging bucket for clamscan

--- video_management/video_processing.py
+++ video_management/video_processing.py
@@ -2,6 +2,7 @@ from aws_cdk import aws_iam
 from aws_cdk import aws_lambda_destinations
 from aws_cdk import aws_s3 as s3
 from cdk_serverless_clamscan import ServerlessClamscan
+from cdk_serverless_clamscan import ServerlessClamscanLoggingProps
 from constructs import Construct
 
 from video_management.lambda_packaging import PackageLambda
@@ -24,7 +25,7 @@ class VideoProcessing:
             upload_bucket=upload_bucket,
             publish_bucket=publish_bucket,
         )
-        self._create_clamscan(upload_bucket)
+        self._create_clamscan(upload_bucket, logging_bucket=logging_bucket)
 
     def _create_publish_lambda(
         self,
@@ -40,11 +41,14 @@ class VideoProcessing:
         self.publish_lambda.add_environment("SOURCE_BUCKET", upload_bucket.bucket_name)
         self.publish_lambda.add_environment("DEST_BUCKET", publish_bucket.bucket_name)
 
-    def _create_clamscan(self, upload_bucket: s3.Bucket):
+    def _create_clamscan(self, upload_bucket: s3.Bucket, logging_bucket: s3.Bucket):
         ServerlessClamscan(
             self._construct,
             "ClamScan",
             buckets=[upload_bucket],
             on_result=aws_lambda_destinations.LambdaDestination(self.publish_lambda),
             on_error=aws_lambda_destinations.LambdaDestination(self.publish_lambda),
+            defs_bucket_access_logs_config=ServerlessClamscanLoggingProps(
+                logs_bucket=logging_bucket, logs_prefix="ClamScan"
+            ),
         )
