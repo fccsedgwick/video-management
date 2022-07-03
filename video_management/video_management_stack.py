c913a1d9e76c61c3d2fdc4c8f1bfd4e12bd459e0
Leland Weathers <97176164+lelandweathers@users.noreply.github.com>
2022-07-03 08:26:02 -0500

    remove uneeded buckets, parameters, roles

--- video_management/video_management_stack.py
+++ video_management/video_management_stack.py
@@ -2,9 +2,10 @@ from aws_cdk import aws_s3 as s3
 from aws_cdk import Stack
 from constructs import Construct
 
-from video_management.video_posting import VideoPosting
-from video_management.video_processing import VideoProcessing
 from video_management.video_storage import VideoStorage
+
+# from video_management.video_posting import VideoPosting
+# from video_management.video_processing import VideoProcessing
 
 
 class VideoManagementStack(Stack):
@@ -14,15 +15,3 @@ class VideoManagementStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         video_storage = VideoStorage(self, logging_bucket)
-
-        video_processing = VideoProcessing(
-            self,
-            logging_bucket=logging_bucket,
-            upload_bucket=video_storage.upload_bucket,
-            publish_bucket=video_storage.publish_bucket,
-            publish_role=video_storage.publish_role,
-        )
-
-        video_publishing = VideoPosting(
-            self, publish_bucket=video_storage.publish_bucket
-        )
--- video_management/video_storage.py
+++ video_management/video_storage.py
@@ -1,12 +1,7 @@
-import json
-from os import getenv
-
 from aws_cdk import aws_cloudfront
 from aws_cdk import aws_cloudfront_origins
 from aws_cdk import aws_iam as iam
 from aws_cdk import aws_s3 as s3
-from aws_cdk import aws_ssm as ssm
-from aws_cdk import RemovalPolicy
 from constructs import Construct
 
 
@@ -15,52 +10,14 @@ class VideoStorage:
         self._construct = construct
 
         self._create_iam()
-        self._create_upload_bucket(logging_bucket)
+        # self._create_upload_bucket(logging_bucket)
         self._create_publish_bucket(logging_bucket)
         self._create_cdn(logging_bucket)
-        self._create_ssm_parameters()
+        # self._create_ssm_parameters()
 
     def _create_iam(self):
         user = iam.User(self._construct, "s3_upload_user")
         self.upload_role = iam.Role(self._construct, "s3_upload_role", assumed_by=user)
-
-        self.publish_role = iam.Role(
-            self._construct,
-            "s3_publish_role",
-            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
-        )
-
-    def _create_upload_bucket(self, logging_bucket: s3.Bucket):
-        self.upload_bucket = s3.Bucket(
-            self._construct,
-            "UploadedVideos",
-            object_ownership=s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
-            enforce_ssl=True,
-            server_access_logs_bucket=logging_bucket,
-            server_access_logs_prefix="s3-upload-bucket-videos",
-            removal_policy=RemovalPolicy.DESTROY,
-            auto_delete_objects=True,
-            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
-        )
-
-        self.upload_bucket.grant_put(self.upload_role)
-
-        self.upload_bucket.grant_read(self.publish_role)
-        self.upload_bucket.grant_delete(self.publish_role)
-
-    def _create_ssm_parameters(self):
-        param_value = {
-            "bucket": self.upload_bucket.bucket_name,
-            "upload_path": "uploads/",
-            "upload_role": self.upload_role.role_arn,
-        }
-        bucket_param = ssm.StringParameter(
-            self._construct,
-            "VideoStorageUploadBucket",
-            parameter_name=getenv("UPLOADPARAMETER"),
-            string_value=json.dumps(param_value),
-        )
-        bucket_param.grant_read(self.upload_role)
 
     def _create_publish_bucket(self, logging_bucket: s3.Bucket):
         self.publish_bucket = s3.Bucket(
@@ -73,8 +30,7 @@ class VideoStorage:
             block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
         )
 
-        self.publish_bucket.grant_put(self.publish_role)
-        self.publish_bucket.grant_read(self.publish_role)
+        self.publish_bucket.grant_write(self.upload_role)
 
     def _create_cdn(self, logging_bucket: s3.Bucket):
         self.cloudfront = aws_cloudfront.Distribution(
