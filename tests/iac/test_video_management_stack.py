9a1553ba5c1d2b6eed751a1a51dee5706bc7f783
Leland Weathers <97176164+lelandweathers@users.noreply.github.com>
2022-07-04 14:38:34 -0500

    remove no longer used tests

--- tests/iac/test_video_management_stack.py
+++ tests/iac/test_video_management_stack.py
@@ -18,34 +18,9 @@ from tests.iac import video_management_stack
 from tests.iac import video_management_template
 
 
-clamscan_known_resource_types = [
-    "AWS::CloudFormation::CustomResource",
-    "AWS::EC2::FlowLog",
-    "AWS::EC2::RouteTable",
-    "AWS::EC2::SecurityGroup",
-    "AWS::EC2::SecurityGroupEgress",
-    "AWS::EC2::SecurityGroupIngress",
-    "AWS::EC2::Subnet",
-    "AWS::EC2::SubnetRouteTableAssociation",
-    "AWS::EC2::VPC",
-    "AWS::EC2::VPCEndpoint",
-    "AWS::EFS::AccessPoint",
-    "AWS::EFS::FileSystem",
-    "AWS::EFS::MountTarget",
-    "AWS::Events::Rule",
-    "AWS::IAM::Policy",
-    "AWS::IAM::Role",
-    "AWS::Lambda::EventInvokeConfig",
-    "AWS::Lambda::Function",
-    "AWS::Lambda::Permission",
-    "AWS::Logs::LogGroup",
-    "AWS::S3::Bucket",
-    "AWS::S3::BucketPolicy",
-    "Custom::S3AutoDeleteObjects",
-]
-
-
 solution_known_resource_types = [
+    "AWS::CloudFront::CloudFrontOriginAccessIdentity",
+    "AWS::CloudFront::Distribution",
     "AWS::IAM::User",
     "AWS::IAM::Policy",
     "AWS::IAM::Role",
@@ -55,11 +30,7 @@ solution_known_resource_types = [
     "AWS::S3::Bucket",
     "AWS::S3::BucketPolicy",
     "AWS::Signer::SigningProfile",
-    "AWS::SQS::Queue",
-    "AWS::SSM::Parameter",
     "Custom::LogRetention",
-    "Custom::S3AutoDeleteObjects",
-    "Custom::S3BucketNotifications",
 ]
 
 
@@ -84,23 +55,9 @@ def non_clamscan_resources_and_types(resources_and_types) -> List[ResourceType]:
     return non_clamscan_resources_and_types
 
 
-@pytest.fixture
-def upload_bucket_name(non_clamscan_resources_and_types):
-    upload_bucket_names = [
-        x.name
-        for x in non_clamscan_resources_and_types
-        if x.type == "AWS::S3::Bucket" and x.name.startswith("UploadedVideos")
-    ]
-    assert len(upload_bucket_names) == 1
-    return upload_bucket_names[0]
-
-
 def test_resources_are_known_types(resources_and_types):
     for x in resources_and_types:
-        if x.name.startswith("ClamScan"):
-            assert x.type in clamscan_known_resource_types
-        else:
-            assert x.type in solution_known_resource_types
+        assert x.type in solution_known_resource_types
 
 
 def test_only_one_iam_user_created(video_management_template):
@@ -138,14 +95,14 @@ def test_roles_created(non_clamscan_resources_and_types):
     solution_roles = [
         x for x in non_clamscan_resources_and_types if x.type == "AWS::IAM::Role"
     ]
-    assert len(solution_roles) == 6
+    assert len(solution_roles) == 1
     roles_startwith = [
         "s3uploadrole",
-        "s3publishrole",
-        "CustomS3AutoDeleteObjectsCustomResourceProviderRole",
-        "LogRetention",
-        "BucketNotificationsHandler",
-        "wppostingrole",
+        # "s3publishrole",
+        # "CustomS3AutoDeleteObjectsCustomResourceProviderRole",
+        # "LogRetention",
+        # "BucketNotificationsHandler",
+        # "wppostingrole",
     ]
     for x in solution_roles:
         found = False
@@ -155,141 +112,3 @@ def test_roles_created(non_clamscan_resources_and_types):
                 found = True
                 break
         assert found
-
-
-def test_upload_bucket_blocks_public_access(
-    upload_bucket_name, video_management_template
-):
-    bucket = video_management_template.to_json()["Resources"][upload_bucket_name]
-    assert bucket["Properties"]["PublicAccessBlockConfiguration"] == {
-        "BlockPublicAcls": True,
-        "BlockPublicPolicy": True,
-        "IgnorePublicAcls": True,
-        "RestrictPublicBuckets": True,
-    }
-
-
-def test_upload_bucket_notifications_to_clamscan_on_object_upload(
-    non_clamscan_resources_and_types,
-    resources_and_types,
-    video_management_template,
-    upload_bucket_name,
-):
-    # There is a notification
-    upload_video_notifications = [
-        x.name
-        for x in non_clamscan_resources_and_types
-        if x.type == "Custom::S3BucketNotifications"
-        and x.name.startswith("UploadedVideosNotifications")
-    ]
-    assert len(upload_video_notifications) == 1
-
-    clamscan_lambdas = [
-        x.name
-        for x in resources_and_types
-        if x.type == "AWS::Lambda::Function"
-        and x.name.startswith("ClamScanServerlessClamscan")
-    ]
-    assert len(clamscan_lambdas) == 1
-    clamscan_lambda = clamscan_lambdas[0]
-
-    # The notification is for object uploads and sends to clamscan
-    video_management_template.has_resource_properties(
-        "Custom::S3BucketNotifications",
-        {
-            "ServiceToken": {"Fn::GetAtt": [Match.any_value(), "Arn"]},
-            "BucketName": {"Ref": upload_bucket_name},
-            "NotificationConfiguration": {
-                "LambdaFunctionConfigurations": [
-                    {
-                        "Events": ["s3:ObjectCreated:*"],
-                        "LambdaFunctionArn": {"Fn::GetAtt": [clamscan_lambda, "Arn"]},
-                    }
-                ]
-            },
-            "Managed": True,
-        },
-    )
-
-
-def test_clamscan_notifications_to_publishing_lambda(
-    non_clamscan_resources_and_types, video_management_template
-):
-    publishing_lambdas = [
-        x.name
-        for x in non_clamscan_resources_and_types
-        if x.type == "AWS::Lambda::Function"
-        and x.name.startswith("VideoPublishingFunction")
-    ]
-    assert len(publishing_lambdas) == 1
-    publishing_lambda = publishing_lambdas[0]
-
-    dead_letter_queues = [
-        x.name
-        for x in non_clamscan_resources_and_types
-        if x.type == "AWS::SQS::Queue" and x.name.startswith("DeadLetterQueue")
-    ]
-    assert len(dead_letter_queues) == 1
-    dead_letter_queue = dead_letter_queues[0]
-
-    video_management_template.has_resource_properties(
-        "AWS::Lambda::EventInvokeConfig",
-        {
-            "FunctionName": {"Ref": Match.any_value()},
-            "Qualifier": "$LATEST",
-            "DestinationConfig": {
-                "OnSuccess": {
-                    "Destination": {"Fn::GetAtt": [publishing_lambda, "Arn"]}
-                },
-                "OnFailure": {
-                    "Destination": {"Fn::GetAtt": [dead_letter_queue, "Arn"]}
-                },
-            },
-        },
-    )
-
-
-def test_published_bucket_notifications_to_posting_lambda(
-    non_clamscan_resources_and_types, video_management_template
-):
-    # There is a notification
-    published_video_notifications = [
-        x.name
-        for x in non_clamscan_resources_and_types
-        if x.type == "Custom::S3BucketNotifications"
-        and x.name.startswith("PublishedVideosNotifications")
-    ]
-    assert len(published_video_notifications) == 1
-
-    publish_bucket = [
-        x.name
-        for x in non_clamscan_resources_and_types
-        if x.type == "AWS::S3::Bucket" and x.name.startswith("PublishedVideos")
-    ][0]
-
-    post_video_lambdas = [
-        x.name
-        for x in non_clamscan_resources_and_types
-        if x.type == "AWS::Lambda::Function"
-        and x.name.startswith("VideoPostingFunction")
-    ]
-    assert len(post_video_lambdas) == 1
-    post_video_lambda = post_video_lambdas[0]
-
-    # The notification is for object uploads and sends to the posting lambda
-    video_management_template.has_resource_properties(
-        "Custom::S3BucketNotifications",
-        {
-            "ServiceToken": {"Fn::GetAtt": [Match.any_value(), "Arn"]},
-            "BucketName": {"Ref": publish_bucket},
-            "NotificationConfiguration": {
-                "LambdaFunctionConfigurations": [
-                    {
-                        "Events": ["s3:ObjectCreated:*"],
-                        "LambdaFunctionArn": {"Fn::GetAtt": [post_video_lambda, "Arn"]},
-                    }
-                ]
-            },
-            "Managed": True,
-        },
-    )
